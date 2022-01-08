93548a47a06131e9b332db0848a351fef0d18be6
gpwaob92679 <gpwaob92679gp@gmail.com>
2022-01-08 15:35:23 +0800

    Complete image rotation correction feature
    - use matplotlib to show figure in debug mode
    - change threshold mode to THRESH_BINARY_INV with threshold value of 100
    - fix angle calculation formula
    - use BORDER_CONSTANT to fill borders with pure white pixels

--- processing.py
+++ processing.py
@@ -1,36 +1,46 @@
 import sys
 
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
+
 # Source: https://www.pyimagesearch.com/2017/02/20/text-skew-correction-opencv-python/
-def calibrate(img):
+def calibrate(img, threshold_val=100, debug=False):
     img = np.array(img)
-    inverted = cv2.bitwise_not(img)
-    thresh = cv2.threshold(inverted, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    thresh = cv2.threshold(img, threshold_val, 255, cv2.THRESH_BINARY_INV)[1]
 
-    # cv2.imshow('thresh', thresh)
-    # cv2.waitKey()
+    # Notice: need to swap the order of two arrays returned from np.where
+    # so that the x and y values will match those in plot/draw parameters
+    coordinates = np.column_stack((np.where(thresh > 0)[1], np.where(thresh > 0)[0]))
+    rect = cv2.minAreaRect(coordinates)
 
-    coordinates = np.column_stack(np.where(thresh > 0))
-    angle = cv2.minAreaRect(coordinates)[-1]
+    if debug:
+        print(rect, file=sys.stderr)
 
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
+        cv2.drawContours(thresh, [np.int0(cv2.boxPoints(rect))], 0, 150, 10)
+        bordered = img
+        cv2.drawContours(bordered, [np.int0(cv2.boxPoints(rect))], 0, 150, 10)
+
+        plt.imshow(thresh, cmap='gray')
+        plt.show()
+        plt.imshow(bordered, cmap='gray')
+        plt.show()
+
+    angle = rect[-1]
+    if angle > 45:
+        angle = angle - 90
 
     (h, w) = img.shape[:2]
     center = (w // 2, h // 2)
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
     rotated = cv2.warpAffine(img, M, (w, h),
-                             flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+                             flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_CONSTANT, borderValue=255)
 
-    print(cv2.minAreaRect(coordinates), file=sys.stderr)
     print("angle: {:.3f}".format(angle), file=sys.stderr)
 
-    return Image.fromarray(img)
+    return Image.fromarray(rotated)
 
 def threshold(img):
     raise NotImplementedError
